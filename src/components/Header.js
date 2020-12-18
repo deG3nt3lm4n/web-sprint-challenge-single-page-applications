487df215caaf449d7151a0bbeaaddf56afafe0f7
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2020-12-18 13:23:20 -0500

    setup completed

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <h2>Lambda Eats</h2>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/pizza'>Help</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
